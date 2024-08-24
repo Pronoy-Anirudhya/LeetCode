fbdd7001dd0e70e4c0f2fb8506acb055b68dbf27
PronoyAnirudhya <anirudhya.sarker@selise.ch>
2024-08-24 22:04:01 +0600

    Added solutio for: 20. Valid Parentheses

---
+++ 20. Valid Parentheses/20. Valid Parentheses/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(new Solution().IsValid("{"));
+Console.ReadLine();
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var parenthesesStack = new Stack<string>();
+        var parenthesesPair = new Dictionary<string, string>
+        {
+            { ")", "(" },
+            { "}", "{" },
+            { "]", "[" }
+        };
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var parentheses = s[i].ToString();
+
+            if (parenthesesPair.ContainsValue(parentheses))
+                parenthesesStack.Push(parentheses);
+            else if (parenthesesStack.Count == 0 || parenthesesStack.Pop() != parenthesesPair[parentheses])
+                return false;
+        }
+
+        return parenthesesStack.Count == 0;
+    }
+}
