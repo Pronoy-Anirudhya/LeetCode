73c2fd0b84934a79655d4864a12d2dad8378fbca
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-08-29 22:16:18 +0600

    Solution for: 875. Koko Eating Bananas

---
+++ 875. Koko Eating Bananas/875. Koko Eating Bananas/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine(new Solution().MinEatingSpeed([30, 11, 23, 4, 20], 5));
+Console.ReadLine();
+
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        var lowestEatingSpeed = 1;
+        var highestEatingSpeed = piles.Max();
+        var minimumEatingSpeed = highestEatingSpeed;
+
+        while (lowestEatingSpeed <= highestEatingSpeed)
+        {
+            var currentEatingSpeed = lowestEatingSpeed + ((highestEatingSpeed - lowestEatingSpeed) / 2);
+            long totalHoursRequiredForCurrentEatingSpeed = 0; //IMPORTANT to keep the variable type long instead of var. Otherwise runtime type will be int for var and then the accumulated value will overflow int capacity eventually producing wrong answer
+
+            foreach (int pile in piles)
+            {
+                var hoursRequiredForThisPile = (int)Math.Ceiling((double)pile / currentEatingSpeed);
+                totalHoursRequiredForCurrentEatingSpeed += hoursRequiredForThisPile;
+            }
+
+            if (totalHoursRequiredForCurrentEatingSpeed <= h)
+            {
+                minimumEatingSpeed = currentEatingSpeed;
+                highestEatingSpeed = currentEatingSpeed - 1;
+            }
+            else
+            {
+                lowestEatingSpeed = currentEatingSpeed + 1;
+            }
+        }
+
+        return minimumEatingSpeed;
+    }
+}
