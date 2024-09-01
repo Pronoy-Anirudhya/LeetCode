af9fc7c1f27843ce9057aa0840130fe3a3c0179a
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-01 23:26:19 +0600

    Solution for: 33. Search in Rotated Sorted Array

---
+++ 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine(new Solution().Search([3, 1], 1));
+Console.ReadLine();
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = left + ((right - left) / 2);
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] >= nums[left])
+            {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
