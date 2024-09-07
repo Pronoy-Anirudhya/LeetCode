8466b158fe6f6a1808ef23cb8366e955821be8f2
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-08 01:48:18 +0600

    Solution for: 121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(new Solution().MaxProfit([7, 2, 5, 1, 1, 1, 1]));
+Console.ReadLine();
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+
+        if (prices.Length == 1)
+            return maxProfit;
+
+        int left = 0, right = 1;
+
+        while (right < prices.Length)
+        {
+            if (prices[left] > prices[right])
+                left = right;
+            else
+            {
+                int currentProfit = prices[right] - prices[left];
+                maxProfit = Math.Max(maxProfit, currentProfit);
+            }
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
