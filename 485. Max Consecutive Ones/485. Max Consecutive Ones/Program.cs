5828c6e931abf2ca9325c88f458b11dd63f9012c
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-17 23:10:18 +0600

    Solution for: 485. Max Consecutive Ones

---
+++ 485. Max Consecutive Ones/485. Max Consecutive Ones/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(new Solution().FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+Console.ReadLine();
+
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int maxConsecutiveOneCount = int.MinValue, currentConsecutiveOneCount = 0;
+
+        for (int index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] == 1)
+                currentConsecutiveOneCount++;
+            else
+            {
+                maxConsecutiveOneCount = Math.Max(maxConsecutiveOneCount, currentConsecutiveOneCount);
+                currentConsecutiveOneCount = 0;
+            }
+        }
+
+        return Math.Max(maxConsecutiveOneCount, currentConsecutiveOneCount);
+    }
+}
