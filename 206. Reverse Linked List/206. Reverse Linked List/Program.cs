1fbc272829a0280f5b4a0431a46c373acdbd8ffc
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-22 01:14:39 +0600

    Solution for: 206. Reverse Linked List

---
+++ 206. Reverse Linked List/206. Reverse Linked List/Program.cs
@@ -0,0 +1,39 @@
+﻿var l5 = new ListNode(5);
+var l4 = new ListNode(4, l5);
+var l3 = new ListNode(3, l4);
+var l2 = new ListNode(2, l3);
+var head = new ListNode(1, l2);
+
+Console.WriteLine(new Solution().ReverseList(head));
+Console.ReadLine();
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null)
+        {
+            var currentNext = current.next;
+            current.next = previous;
+            previous = current;
+            current = currentNext;
+        }
+
+        return previous;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
