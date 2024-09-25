76e0367270527b59c42c14704666efc2e5e51464
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-25 23:35:28 +0600

    Updated solution for: 2. Add Two Numbers

--- 2. Add Two Numbers/2. Add Two Numbers/Program.cs
+++ 2. Add Two Numbers/2. Add Two Numbers/Program.cs
@@ -1,69 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//var l1 = new ListNode(2);
-//var l1_1 = new ListNode(4);
-//var l1_2 = new ListNode(3);
-//l1.next = l1_1;
-//l1_1.next = l1_2;
-
-//var l2 = new ListNode(5);
-//var l2_1 = new ListNode(6);
-//var l2_2 = new ListNode(4);
-//l2.next = l2_1;
-//l2_1.next = l2_2;
-
-var l1 = new ListNode(9);
-var l1_1 = new ListNode(9);
-var l1_2 = new ListNode(9);
+﻿var l1 = new ListNode(2);
+var l1_1 = new ListNode(4);
+var l1_2 = new ListNode(3);
 l1.next = l1_1;
 l1_1.next = l1_2;
 
-var l2 = new ListNode(9);
+var l2 = new ListNode(5);
+var l2_1 = new ListNode(6);
+var l2_2 = new ListNode(4);
+l2.next = l2_1;
+l2_1.next = l2_2;
 
-new Solution().AddTwoNumbers(l1, l2);
+//var l1 = new ListNode(9);
+//var l1_1 = new ListNode(9);
+//var l1_2 = new ListNode(9);
+//l1.next = l1_1;
+//l1_1.next = l1_2;
 
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
+//var l2 = new ListNode(9);
+
+Console.WriteLine(new Solution().AddTwoNumbers(l1, l2));
+Console.ReadLine();
 
 public class Solution
 {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        var result = new ListNode();
-        var head = result;
-        var carry = 0;
+        ListNode head = new(), currentNode = head;
+        int carry = 0;
 
-        while (l1 != null || l2 != null)
+        while (l1 != null || l2 != null || carry != 0)
         {
-            var l1Value = l1 != null ? l1.val : 0;
-            var l2Value = l2 != null ? l2.val : 0;
+            int l1Value = l1 != null ? l1.val : 0;
+            int l2Value = l2 != null ? l2.val : 0;
 
-            var sum = (carry + l1Value + l2Value) % 10;
-            carry = (carry + l1Value + l2Value) / 10;
+            int currentSum = l1Value + l2Value + carry;
+            int numberToAddAfterCarry = currentSum % 10;
+            carry = currentSum / 10;
+            currentNode.next = new(numberToAddAfterCarry);
+            currentNode = currentNode.next;
+
             l1 = l1?.next;
             l2 = l2?.next;
-
-            result.next = new ListNode(sum);
-            result = result.next;
-        };
-
-        if (carry == 1)
-        {
-            result.next = new ListNode(carry);
-            result = result.next;
         }
 
-        head = head.next;
-        return head;
+        return head.next;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
     }
 }
