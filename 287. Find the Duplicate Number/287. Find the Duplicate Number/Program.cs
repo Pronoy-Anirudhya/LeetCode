f47a8bf7624269a37c10008f4a2d1afcecf984d0
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-09-28 00:07:17 +0600

    Solution for: 287. Find the Duplicate Number

--- 287. Find the Duplicate Number/287. Find the Duplicate Number/Program.cs
+++ 287. Find the Duplicate Number/287. Find the Duplicate Number/Program.cs
@@ -1,12 +1,26 @@
-﻿var nums = new int[] { 3, 1, 3, 4, 2 };
-Console.WriteLine(new Solution().FindDuplicate(nums));
+﻿Console.WriteLine(new Solution().FindDuplicate([3, 1, 3, 4, 2]));
 Console.ReadLine();
 
 public class Solution
 {
     public int FindDuplicate(int[] nums)
     {
+        int slow = 0, fast = 0;
 
-        return 0;
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = 0;
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 }
