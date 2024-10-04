14b37d852c76d5de49e941cc28f7f3fa84184d5e
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-10-05 03:45:29 +0600

    Solution for: 98. Validate Binary Search Tree

---
+++ 98. Validate Binary Search Tree/98. Validate Binary Search Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+public class Solution
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        return IsValidBSTSubtree(root, double.NegativeInfinity, double.PositiveInfinity);
+    }
+
+    private bool IsValidBSTSubtree(TreeNode node, double floorValue, double ceilingValue)
+    {
+        if (node == null)
+            return true;
+
+        if (!(node.val > floorValue && node.val < ceilingValue))
+            return false;
+
+        return IsValidBSTSubtree(node.left, floorValue, node.val) && IsValidBSTSubtree(node.right, node.val, ceilingValue);
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
