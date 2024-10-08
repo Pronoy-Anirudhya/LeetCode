16ce96923c754fa1c5840c87879df66050d8bfb0
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-10-09 00:35:38 +0600

    Solution for: 102. Binary Tree Level Order Traversal

---
+++ 102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        if (root == null)
+            return [];
+
+        List<IList<int>> nodes = [];
+        Queue<TreeNode> bfsQueue = [];
+        bfsQueue.Enqueue(root);
+
+        while (bfsQueue.Count > 0)
+        {
+            int nodeCountInQueue = bfsQueue.Count;
+            List<int> nodesInCurrentLevel = [];
+
+            while (nodeCountInQueue-- > 0)
+            {
+                TreeNode node = bfsQueue.Dequeue();
+                nodesInCurrentLevel.Add(node.val);
+
+                if (node.left != null)
+                    bfsQueue.Enqueue(node.left);
+
+                if (node.right != null)
+                    bfsQueue.Enqueue(node.right);
+            }
+
+            nodes.Add(nodesInCurrentLevel);
+        }
+
+        return nodes;
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
