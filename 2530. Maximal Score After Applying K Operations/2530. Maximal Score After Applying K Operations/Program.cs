b07ff81c01c69cb3826772285c99b4338320342a
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-10-14 19:54:24 +0600

    Solution for: 2530. Maximal Score After Applying K Operations

---
+++ 2530. Maximal Score After Applying K Operations/2530. Maximal Score After Applying K Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+public class Solution
+{
+    public long MaxKelements(int[] nums, int k)
+    {
+        long answer = 0;
+        PriorityQueue<int, int> maxHeap = new(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+        for (int index = 0; index < nums.Length; index++)
+            maxHeap.Enqueue(nums[index], nums[index]);
+
+        while (k-- > 0 && maxHeap.Count > 0)
+        {
+            int currentElement = maxHeap.Dequeue();
+            answer += currentElement;
+            currentElement = (int)Math.Ceiling((decimal)currentElement / 3);
+            maxHeap.Enqueue(currentElement, currentElement);
+        }
+
+        return answer;
+    }
+}
