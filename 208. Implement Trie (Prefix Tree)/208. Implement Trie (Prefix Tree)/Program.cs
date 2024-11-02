d024674ac654b2290ce20358ef2bafac9709b080
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-11-02 17:59:18 +0600

    Solution for: 208. Implement Trie (Prefix Tree)

---
+++ 208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree)/Program.cs
@@ -0,0 +1,82 @@
+﻿Trie trie = new Trie();
+trie.Insert("apple");
+Console.WriteLine(trie.Search("apple"));   // return True
+Console.WriteLine(trie.Search("app"));     // return False
+Console.WriteLine(trie.StartsWith("app")); // return True
+trie.Insert("app");
+Console.WriteLine(trie.Search("app"));     // return True
+
+Console.ReadLine();
+
+public class Trie
+{
+    private TrieNode Root;
+
+    public Trie()
+    {
+        Root = new();
+    }
+
+    public void Insert(string word)
+    {
+        TrieNode current = Root;
+
+        foreach (char character in word.ToCharArray())
+        {
+            int index = character - 'a';
+
+            if (current.Children[index] == null)
+                current.Children[index] = new();
+
+            current = current.Children[index];
+        }
+
+        current.IsEndOfAWord = true;
+    }
+
+    public bool Search(string word)
+    {
+        TrieNode current = Root;
+
+        foreach (char character in word.ToCharArray())
+        {
+            int index = character - 'a';
+
+            if (current.Children[index] == null)
+                return false;
+
+            current = current.Children[index];
+        }
+
+        return current.IsEndOfAWord;
+    }
+
+    public bool StartsWith(string prefix)
+    {
+        TrieNode current = Root;
+
+        foreach (char character in prefix.ToCharArray())
+        {
+            int index = character - 'a';
+
+            if (current.Children[index] == null)
+                return false;
+
+            current = current.Children[index];
+        }
+
+        return true;
+    }
+}
+
+public class TrieNode
+{
+    public TrieNode()
+    {
+        Children = new TrieNode[26];
+        IsEndOfAWord = false;
+    }
+
+    public TrieNode[] Children { get; set; }
+    public bool IsEndOfAWord { get; set; }
+}
