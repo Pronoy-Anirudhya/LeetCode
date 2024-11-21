3df3638e34ced9f4261055bf1c2252ac9e0d7ca5
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-11-22 02:53:29 +0600

    Solution for: 2526. Find Consecutive Integers from a Data Stream

---
+++ 2526. Find Consecutive Integers from a Data Stream/2526. Find Consecutive Integers from a Data Stream/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+public class DataStream
+{
+    private int Value, K, Index, LastNonValueIndex;
+
+    public DataStream(int value, int k)
+    {
+        Value = value;
+        K = k;
+        Index = 0;
+        LastNonValueIndex = -1;
+    }
+
+    public bool Consec(int num)
+    {
+        Index += 1;
+
+        if (num != Value)
+            LastNonValueIndex = Index;
+
+        int validStreamLength = LastNonValueIndex != -1 ? Index - LastNonValueIndex : Index;
+        return validStreamLength >= K;
+    }
+}
