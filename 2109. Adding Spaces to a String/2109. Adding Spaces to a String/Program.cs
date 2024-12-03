5490ddd844e5fe3a9ed98729426bb54b30fa3d1c
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-12-03 20:53:24 +0600

    Solution for: 2109. Adding Spaces to a String

---
+++ 2109. Adding Spaces to a String/2109. Adding Spaces to a String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+Console.WriteLine(new Solution().AddSpaces("LeetcodeHelpsMeLearn", [8, 13, 15]));
+Console.ReadLine();
+
+public class Solution
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        StringBuilder result = new();
+        int stringIndex = 0, spaceIndex = 0;
+
+        while (stringIndex < s.Length || spaceIndex < spaces.Length)
+        {
+            if (spaceIndex < spaces.Length && stringIndex == spaces[spaceIndex])
+            {
+                result.Append(' ');
+                spaceIndex++;
+            }
+            else if (stringIndex < s.Length)
+            {
+                result.Append(s[stringIndex]);
+                stringIndex++;
+            }
+        }
+
+        return result.ToString();
+    }
+}
