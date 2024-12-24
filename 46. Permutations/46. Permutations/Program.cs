302ed1dc5f386967db848e8c63a532a06acf5afe
PronoyAnirudhya <pro_adroit@yahoo.com>
2024-12-25 00:03:15 +0600

    Solution for: 46. Permutations

---
+++ 46. Permutations/46. Permutations/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(new Solution().Permute([1, 2, 3]));
+Console.ReadLine();
+
+public class Solution
+{
+    public int Count = 1;
+
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        IList<IList<int>> permutations = [];
+        Backtracking(permutations, [], nums);
+        return permutations;
+    }
+
+    private void Backtracking(IList<IList<int>> result, List<int> resultUntilNow, int[] nums)
+    {
+        if (resultUntilNow.Count == nums.Length)
+        {
+            result.Add(new List<int>(resultUntilNow));
+            return;
+        }
+
+        foreach (int number in nums)
+        {
+            if (resultUntilNow.Contains(number))
+                continue;
+
+            resultUntilNow.Add(number);
+            Backtracking(result, resultUntilNow, nums);
+            resultUntilNow.Remove(number);
+        }
+    }
+}
