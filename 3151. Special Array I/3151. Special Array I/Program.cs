88a60fd17b116e7daa0422194c056f91dcb2e07c
PronoyAnirudhya <pro_adroit@yahoo.com>
2025-02-01 20:18:08 +0600

    Solution for: 3151. Special Array I

---
+++ 3151. Special Array I/3151. Special Array I/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(new Solution().IsArraySpecial([4, 3, 1, 6]));
+Console.ReadLine();
+
+public class Solution
+{
+    public bool IsArraySpecial(int[] nums)
+    {
+        int left = 0, right = 1;
+
+        while (right < nums.Length)
+        {
+            if (nums[left] % 2 == nums[right] % 2)
+                return false;
+
+            left++;
+            right++;
+        }
+
+        return true;
+    }
+}
