67540b75878884af1c103ed83a533d15470f6c0d
PronoyAnirudhya <pro_adroit@yahoo.com>
2025-08-13 20:31:11 +0600

    Solution for: 326. Power of Three

---
+++ 326. Power of Three/326. Power of Three/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(new Solution().IsPowerOfThree(29));
+Console.ReadLine();
+
+public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n > 1)
+        {
+            while (n % 3 == 0)
+                n /= 3;
+        }
+
+        // If n is 1, it means it is a power of 3 (3^0 = 1)
+        return n == 1;
+
+        // Alternative O(1) Solution: Since 3^19 is the largest power of 3 that fits in an int, we can also check if n is a divisor of 3^19. This algorithm only works for positive prime numbers.
+        //return n > 0 && Math.Pow(3,19) % n==0;
+    }
+}
