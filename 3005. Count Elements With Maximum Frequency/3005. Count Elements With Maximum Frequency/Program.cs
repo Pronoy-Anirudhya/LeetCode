9a42701b4fd7f1ea5a129b8479a4bc6199a5699a
PronoyAnirudhya <pro_adroit@yahoo.com>
2025-09-22 15:26:03 +0600

    Solution for: 3005. Count Elements With Maximum Frequency

---
+++ 3005. Count Elements With Maximum Frequency/3005. Count Elements With Maximum Frequency/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(new Solution().MaxFrequencyElements([1, 2, 2, 3, 1, 4]));
+Console.ReadLine();
+
+public class Solution
+{
+    public int MaxFrequencyElements(int[] nums)
+    {
+        int result = 0, maxFrequency = int.MinValue;
+        int[] frequency = new int[101];
+
+        foreach (int num in nums)
+        {
+            frequency[num]++;
+            maxFrequency = Math.Max(maxFrequency, frequency[num]); // Track the maximum frequency.
+        }
+
+        for (int index = 100; index > 0; index--)
+        {
+            // Count how many elements have the maximum frequency and add to the result.
+            if (frequency[index] == maxFrequency)
+                result += maxFrequency;
+        }
+
+        return result;
+    }
+}
