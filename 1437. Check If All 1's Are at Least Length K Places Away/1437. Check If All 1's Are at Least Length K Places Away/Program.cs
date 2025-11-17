0334e7c49e82b9bf3079d41e3e6bc100a40c9f2f
PronoyAnirudhya <pro_adroit@yahoo.com>
2025-11-17 20:08:39 +0600

    Solution for: 1437. Check If All 1's Are at Least Length K Places Away

---
+++ 1437. Check If All 1's Are at Least Length K Places Away/1437. Check If All 1's Are at Least Length K Places Away/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(new Solution().KLengthApart([1, 0, 0, 1, 0, 1], 2));
+Console.ReadLine();
+
+public class Solution
+{
+    public bool KLengthApart(int[] nums, int k)
+    {
+        int lastOneIndex = -1;
+
+        for (int index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] == 1)
+            {
+                // First one found. Just record its index.
+                if (lastOneIndex == -1)
+                {
+                    lastOneIndex = index;
+                    continue; // Move to the next index. No need to check distance for the first one.
+                }
+
+                // Check the distance between the current one and the last one. If it's less than k, return false. (index - lastOneIndex - 1) is the number of zeros between two ones. We subtract 1 because we want the distance between the ones, not including the ones themselves.
+                if (index - lastOneIndex - 1 < k)
+                    return false;
+
+                lastOneIndex = index;
+            }
+        }
+
+        return true; // All ones are at least k places away from each other. Return true.
+    }
+}
