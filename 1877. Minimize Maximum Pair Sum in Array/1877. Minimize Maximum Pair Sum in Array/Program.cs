6f1ce298b1305ac9beaa0103b5671f58c74b9a7d
PronoyAnirudhya <pro_adroit@yahoo.com>
2026-01-24 20:10:28 +0600

    Solution for: 1877. Minimize Maximum Pair Sum in Array

---
+++ 1877. Minimize Maximum Pair Sum in Array/1877. Minimize Maximum Pair Sum in Array/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(new Solution().MinPairSum([3, 4, 6, 7]));
+Console.ReadLine();
+
+public class Solution
+{
+    public int MinPairSum(int[] nums)
+    {
+        int minimumPairSum = int.MinValue; // Initialize to the smallest possible integer value.
+        Array.Sort(nums); // Sort the array in ascending order. This helps in pairing the smallest and largest elements together.
+
+        for (int left = 0, right = nums.Length - 1; left < right; left++, right--)
+        {
+            int currentPairSum = nums[left] + nums[right];
+            minimumPairSum = Math.Max(minimumPairSum, currentPairSum); // Update the maximum pair sum found so far.
+        }
+
+        return minimumPairSum;
+    }
+}
